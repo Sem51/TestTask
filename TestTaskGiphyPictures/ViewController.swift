0d2026f46c93d0f0ad33a2630a30aa1093145051
Sem51 <semjono@rambler.ru>
2016-11-23 23:05:24 +0300

    Initial Commit

---
+++ TestTaskGiphyPictures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTaskGiphyPictures
+//
+//  Created by Семен Осипов on 23.11.16.
+//  Copyright © 2016 Семен Осипов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
