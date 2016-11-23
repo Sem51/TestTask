dd7e0fe080a0722e2593e61fe8ad45d1ba0b5a50
Sem51 <semjono@rambler.ru>
2016-11-23 23:31:38 +0300

    FirstCommit

---
+++ RequestService.swift
@@ -0,0 +1,22 @@
+//
+//  RequestService.swift
+//  TestTaskGiphyPictures
+//
+//  Created by Семен Осипов on 23.11.16.
+//  Copyright © 2016 Семен Осипов. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Request {
+    
+    let giphyUrl = "http://api.giphy.com/v1/gifs/random"
+    let publicBetaApikey = "dc6zaTOxFJmzC"
+    let format = "json"
+    
+//    func getRandomImage() -> UIImage {
+//        
+//    }
+    
+}
